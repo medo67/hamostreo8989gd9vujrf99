dd2258ac85c22149399591bd34dde643b13cc232
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-19 03:08:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,26 @@ client.user.setGame(`JR server`,"https://www.twitch.tv/ledom999")
   console.log('')
   console.log('')
 });
- 
+ client.on('message', message => {
+   let embed = new Discord.RichEmbed()
+
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+if(message.content.split(' ')[0] == '*bc') {
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
+        message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+.addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
+            .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
+    .setColor('#ff0000')
+                 .addField('ّ', args)
+            m.send(``,{embed: bc});
+});
  
  
 client.login(process.env.BOT_TOKEN);
