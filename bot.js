5233335c1ce6bd1533bf1c4e8fd30b87329a41a3
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-19 08:52:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,28 +30,21 @@ client.user.setGame(`JR server`,"https://www.twitch.tv/ledom999")
   console.log('')
   console.log('')
 });
- const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`on 0 servers | *help | by aslan mal codes .`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
-});
+
 client.on('message', message => {
-   let embed = new Discord.RichEmbed()
+   let embed = new discord.RichEmbed()
 
     let args = message.content.split(' ').slice(1).join(' ');
      if(!message.channel.guild) return;
 if(message.content.split(' ')[0] == '*bc') {
          message.react("✔️")
-          let embed = new Discord.RichEmbed()
+          let embed = new discord.RichEmbed()
     .setColor("#FF00FF")
     .setThumbnail(message.author.avatarURL)   
                                       .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
                  message.channel.sendEmbed(embed);
         message.guild.members.forEach(m => {
-            var bc = new Discord.RichEmbed()
+            var bc = new discord.RichEmbed()
 .addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
             .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
     .setColor('#ff0000')
